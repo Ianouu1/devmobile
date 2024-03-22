9d95e16134c4a4f41ca9d0a25884ad82feac6680
Florian <courrierdsf@gmail.com>
2024-03-22 11:25:58 +0100

    TP1 - Added onStart,onResume,onPause,onRestart,onStop,OnDestroy

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -27,6 +28,50 @@ public class MainActivity extends AppCompatActivity {
 
         bouton = findViewById(R.id.button);
         textView = findViewById(R.id.textView);
+        Log.i("OnCreate()","onCreate");
+        textView.setText("OnCreate()  \n");
         bouton.setOnClickListener(v -> textView.setText(R.string.toto));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("onStart()","onstart()");
+        textView.append("onStart() \n");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("onResume()","onresume()");
+        textView.append("onResume() \n");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("onPause()", "onPause()");
+        textView.append("onPause() \n");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("onRestart()", "onRestart()");
+        textView.append("onRestart() \n");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("onStop()", "onStop()");
+        textView.append("onStop() \n");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("onDestroy()", "onDestroy()");
+        textView.append("onDestroy() \n");
     }
 }
